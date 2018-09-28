a78bff15f3919eeec2abce893ef197822789b6fc
Eric Rohner <ericrohner22@gmail.com>
2018-09-27 23:55:54 -0600

    new button started

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import * as routes from '../constants/routes'
 import './Navigation.css'
 
-const Navigation = () =>
+class Navigation extends Component {
+  //{/*<nav>*/}
 
-  <nav>
-    <button><Link to={ routes.LANDING }>Landing</Link></button>
-    <button><Link to={ routes.PAGE1 }>Page1</Link></button>
-    <button><Link to={ routes.PAGE2 }>Page2</Link></button>
-  </nav>
+//{/*<button><Link to={ routes.LANDING }>Landing</Link></button>*/}
+//{/*<button><Link to={ routes.PAGE1 }>Page1</Link></button>*/}
+//{/*<button><Link to={ routes.PAGE2 }>Page2</Link></button>*/}
+//{/*</nav>*/}
+render(){
+  return (
+    <body>
+      <a href="/"> Home </a>
+    </body>
+  )
+}
+}
 
 export default Navigation
